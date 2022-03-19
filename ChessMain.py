5b48fe68ce9687e07a4520f3c004634552a7b7db
couturepatwit <56851517+couturepatwit@users.noreply.github.com>
2022-03-19 14:46:45 -0400

    Update ChessMain.py
    Added ability to move with player clicks

--- ChessMain.py
+++ ChessMain.py
@@ -25,20 +25,40 @@ Main Driver. Handle user input and updating graphics.
 '''
 def main():
     p.init()
-
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
     loadImages()
     running = True
+    sqSelected = ()
+    playerClicks = []
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row,col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row,col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
+        p.display.set_caption('Chess Bot')
 
 #Used for all graphics for current game state
 def drawGameState(screen, gs):
@@ -52,6 +72,7 @@ def drawBoard(screen):
         for c in range(DIMENSION):
             color = colors[((r+c)%2)]
             p.draw.rect(screen, color, p.Rect(c*SQ_SIZE,r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
 
 # to draw pieces on the board
 def drawPieces(screen, board):
