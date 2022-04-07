3fe9c4ebb2e71e20c9ddf5e7d59f277d8636ada3
petersonm4atwit <60017270+petersonm4atwit@users.noreply.github.com>
2022-04-07 11:01:31 -0400

    Update ChessMain.py

--- ChessMain.py
+++ ChessMain.py
@@ -1,9 +1,9 @@
 """
 Main Driver File. Responsible for User Input and displaying current GameState object.
 """
-
+from Chessnut import Game
 import pygame as p
-from Chess import ChessEngine
+import ChessEngine
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -15,15 +15,17 @@ IMAGES = {}
 Initialize a global dictionary of images. Called only once to save resources.
 '''
 def loadImages():
-    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    pieces = ['wR', 'wp', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("Chess/chesspieces/images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+        #IMAGES[piece] = p.transform.scale(p.image.load("C:/Users/mpete/Documents/ChessBot/chesspieces/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     #access image by calling IMAGES['wp']
 
 '''
 Main Driver. Handle user input and updating graphics.
 '''
 def main():
+    chessgame = Game()
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
@@ -47,12 +49,23 @@ def main():
                 else:
                     sqSelected = (row,col)
                     playerClicks.append(sqSelected)
+                    #here
+                    rowRank = get_Row(row)
+                    colRank = get_Col(col)
+                    rowColRank = rowRank+colRank
+                    #print(rowColRank)
+                    allmoves = chessgame.get_moves()
+                    posmoves = [x for x in allmoves if rowColRank in x]
+                    print("All Posible moves for "+ rowColRank + " are: ")
+                    print(posmoves)
+                    
                 if len(playerClicks) == 2:
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    gs.makeMove(move, chessgame)
                     sqSelected = ()
                     playerClicks = []
+
 
 
         drawGameState(screen, gs)
@@ -81,7 +94,16 @@ def drawPieces(screen, board):
             piece = board[r][c]
             if piece != "--":
                 screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE,r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                
+ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
 
-
-
+def get_Row(val):
+    for key, value in ranksToRows.items():
+        if val == value:
+            return key
+def get_Col(val):
+    for key, value in filesToCols.items():
+        if val == value:
+            return key
 main()
