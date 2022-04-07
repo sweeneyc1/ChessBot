82c657195c352cc0e2c90daf5011a191df1aeb21
petersonm4atwit <60017270+petersonm4atwit@users.noreply.github.com>
2022-04-07 11:02:17 -0400

    Update ChessEngine.py

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,6 +1,7 @@
 """
 Responsible for storing information about the current state of a chess game and determining valid moves and log them.
 """
+
 class GameState():
     def __init__(self):
         #8x8 2d list, each element has 2 characters.
@@ -19,28 +20,38 @@ class GameState():
         self.whiteToMove = True
         self.moveLog = []
 
-    def makeMove(self, move):
-        print(move)
-        if self.whiteToMove: 
+    def makeMove(self, move, chessgame):
+        #print(chessgame.get_moves())
+        if self.whiteToMove:
                 if self.board[move.startRow][move.startCol][0] == "w":
-                    self.board[move.startRow][move.startCol] = "--"
-                    self.board[move.endRow][move.endCol] = move.pieceMoved
-                    self.moveLog.append(move)
-                    self.whiteToMove = False
+                    try:
+                        chessgame.apply_move(move.getChessNotation())
+                    except:
+                        print("Illegal Move Try Again")
+                    else:
+                        self.board[move.startRow][move.startCol] = "--"
+                        self.board[move.endRow][move.endCol] = move.pieceMoved
+                        self.moveLog.append(move)
+                        self.whiteToMove = False
+                        print("Black Turn")
         if self.whiteToMove == False: 
                 if self.board[move.startRow][move.startCol][0] == "b":
-                    self.board[move.startRow][move.startCol] = "--"
-                    self.board[move.endRow][move.endCol] = move.pieceMoved
-                    self.moveLog.append(move)
-                    self.whiteToMove = True
+                    try:
+                        chessgame.apply_move(move.getChessNotation())
+                    except:
+                        print("Illegal Move Try Again")
+                    else:
+                        self.board[move.startRow][move.startCol] = "--"
+                        self.board[move.endRow][move.endCol] = move.pieceMoved
+                        self.moveLog.append(move)
+                        self.whiteToMove = True
+                        print("White Turn")
 
 class Move():
 
-    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
-                   "5": 3, "6": 2, "7": 1, "8": 0}
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {v: k for k, v in ranksToRows.items()}
-    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
-                   "e": 4, "f": 5, "g": 6, "h": 7}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
     colsToFiles = {v: k for k, v in filesToCols.items()}
 
     def __init__(self, startSq, endSq, board):
@@ -51,10 +62,14 @@ class Move():
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
 
+        
+    
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
 
 
     def getRankFile(self, r, c):
         return self.colsToFiles[c] + self.rowsToRanks[r]
+
+    
 
